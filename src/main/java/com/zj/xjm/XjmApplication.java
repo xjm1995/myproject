57a08633d4298fef4e840e12dbbd22c06c941b03
xjm1995 <1159040363@qq.com>
2019-10-09 15:58:00 +0800

    create project

---
+++ src/main/java/com/zj/xjm/XjmApplication.java
@@ -0,0 +1,13 @@
+package com.zj.xjm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XjmApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(XjmApplication.class, args);
+    }
+
+}
